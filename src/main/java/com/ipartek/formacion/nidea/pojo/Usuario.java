7fa4f444649c3e2156a832779a15cf0244e1f048
Curso <Curso@Formacion-TOS11.ipartekaula.com>
2018-04-16 14:24:51 +0200

    nuevo

--- src/main/java/com/ipartek/formacion/nidea/pojo/Usuario.java
+++ src/main/java/com/ipartek/formacion/nidea/pojo/Usuario.java
@@ -11,6 +11,8 @@ public class Usuario {
 		super();
 		this.id = -1;
 		this.nombre = "";
+		this.password = "";
+		this.id_rol = -1;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -28,6 +30,22 @@ public class Usuario {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId_rol() {
+		return id_rol;
+	}
+
+	public void setId_rol(int id_rol) {
+		this.id_rol = id_rol;
 	}
 
 	@Override
